7831e7df1709f89423e17239b6b0e0585907852c
Iva Gamilec <gamileciva@gmail.com>
2025-01-05 00:02:46 +0100

    semantic tags i components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <header>
+
+      </header>
+      <nav>
+
+      </nav>
+      <main>
+
+      </main>
+      <footer>
+        
+      </footer>
+    </>
   );
 }
 
---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <nav>
+            <a href="/">
+            <img src={} alt="logo" />
+            </a>
+        </nav>
+    );
+};
+
+export default Nav;
